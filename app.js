04c9707d30d06f631de5fa6806acfd3416908a5f
Max Thakur <MaxThakurCodes@gmail.com>
2020-05-13 05:16:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-\\\\const Discord = require("discord.js");
+const Discord = require("discord.js");
 const readline = require("readline");
 const { CommandHandler } = require("djs-commands");
 const client = new Discord.Client();
